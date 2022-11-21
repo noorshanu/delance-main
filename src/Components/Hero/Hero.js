475959a1f44f8148843d39a93a942549335e7a8a
noor alam <51040978+noorshanu@users.noreply.github.com>
2022-11-21 15:00:50 +0530

    new

--- src/Components/Hero/Hero.js
+++ src/Components/Hero/Hero.js
@@ -1,8 +1,29 @@
 import React from 'react'
+import './Hero.css'
 
 function Hero() {
   return (
-    <div>Hero</div>
+    <>
+    <section className='hero'>
+      <div className='container'>
+        <div className='row justify-content-center'>
+        <div className='col-md-6'>
+          <h1>Creating space for better world to work together</h1>
+          <div className='hero-btn'>
+            <a href="/">Sign up</a>
+            <a href="/">Explore</a>
+          </div>
+        </div>
+        <div className='col-md-6 text-end align-items-center'>
+          <img className='hero-img' src='https://ik.imagekit.io/cforcrypto/Dework/image_1.png?ik-sdk-version=javascript-1.4.3&updatedAt=1669013136326' alt=""/>
+          
+        </div>
+        </div>
+   
+        
+      </div>
+    </section>
+    </>
   )
 }
 
