abac9b1827e55ab3f36115ae1ac9071d4f59bebb
noor alam <51040978+noorshanu@users.noreply.github.com>
2022-11-22 01:49:38 +0530

    Update Feauters.js

--- src/Components/Features/Feauters.js
+++ src/Components/Features/Feauters.js
@@ -24,6 +24,32 @@ function Feauters() {
               <p>Fast, efficient & easy the way blockchain is meant to be</p>
             </div>
           </div>
+          <div className="col-md-4">
+            <div className="feat-box">
+              <img
+                src="https://ik.imagekit.io/cforcrypto/Dework/icons/Group_31.svg?ik-sdk-version=javascript-1.4.3&updatedAt=1669061373660"
+                alt=""
+              />
+              <h3>Tranceparency</h3>
+              <p>
+              By implementing an automated dispute system and reputation stored on the blockchain.{" "}
+              </p>
+              <p>we’re building a trustworthy platform all stakeholders can rely on.</p>
+            </div>
+          </div>
+          <div className="col-md-4">
+            <div className="feat-box">
+              <img
+                src="https://ik.imagekit.io/cforcrypto/Dework/icons/Group_31.svg?ik-sdk-version=javascript-1.4.3&updatedAt=1669061373660"
+                alt=""
+              />
+              <h3>Escrow</h3>
+              <p>
+              With escrow, Freelancers have the guarantee of payment for their services, while the Employer’s funds are safe until the Freelancer produces the expected work.{" "}
+              </p>
+              <p> As it’s based on smart contracts, escrow can’t be manipulated in any way, and makes trust.</p>
+            </div>
+          </div>
         </div>
       </div>
     </section>
