cb168f45a000dea3c35f83ec25c17c385783c5ff
noor alam <51040978+noorshanu@users.noreply.github.com>
2022-11-22 01:33:41 +0530

    new update

--- src/Components/About/About.js
+++ src/Components/About/About.js
@@ -1,4 +1,5 @@
 import React from "react";
+import './About.css'
 
 function About() {
   return (
@@ -7,6 +8,44 @@ function About() {
         <div className="row">
           <div className="col-md-6">
             <h1>About Us</h1>
+            <div className="about-topic">
+              <div className="d-flex">
+                <div>
+                <img src="https://ik.imagekit.io/cforcrypto/Dework/Rectangle_21.png?ik-sdk-version=javascript-1.4.3&updatedAt=1669013135494" alt=""/>
+                </div>
+                <div>
+                  <p className="ab-p1">World’s First </p>
+                  <p className="ab-p2">Fully Decentralized</p>
+                  <p className="ab-p3">Freelance Network</p>
+                </div>
+                
+              </div>
+
+              <div className="d-flex">
+                <div>
+                  <img src="https://ik.imagekit.io/cforcrypto/Dework/Rectangle_21.png?ik-sdk-version=javascript-1.4.3&updatedAt=1669013135494" alt=""/>
+                </div>
+                <div>
+                  <p className="ab-p1">World’s First </p>
+                  <p className="ab-p2">Fully Decentralized</p>
+                  <p className="ab-p3">Freelance Network</p>
+                </div>
+                
+              </div>
+
+              <div className="d-flex">
+                <div>
+                <img src="https://ik.imagekit.io/cforcrypto/Dework/Rectangle_21.png?ik-sdk-version=javascript-1.4.3&updatedAt=1669013135494" alt=""/>
+                </div>
+                <div>
+                  <p className="ab-p1">World’s First </p>
+                  <p className="ab-p2">Fully Decentralized</p>
+                  <p className="ab-p3">Freelance Network</p>
+                </div>
+                
+              </div>
+              
+            </div>
           </div>
 
           <div className="col-md-6">
@@ -17,10 +56,16 @@ function About() {
                 enim ad minim veniam, quis nostrud exercitation ullamco laboris
                 nisi ut aliquip ex ea commodo consequat.{" "}
               </p>
+              <img src="https://ik.imagekit.io/cforcrypto/Dework/Rectangle_103.png?ik-sdk-version=javascript-1.4.3&updatedAt=1669060785239" alt=""/>
+              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+                enim ad minim veniam, quis nostrud exercitation ullamco laboris
+                nisi ut aliquip ex ea commodo consequat.</p>
             </div>
           </div>
         </div>
       </div>
+      <img src="https://ik.imagekit.io/cforcrypto/Dework/WhatsApp_Image_2022-11-05_at_8.21_1__Traced_.png?ik-sdk-version=javascript-1.4.3&updatedAt=1669060785433" alt="" className="side-about"/>
     </section>
   );
 }
