22f6ec75672fc9239ac18390d1288855b7fce1a3
noor alam <51040978+noorshanu@users.noreply.github.com>
2022-11-25 00:46:43 +0530

    de work

--- src/Components/WhyUs/WhyUs.js
+++ src/Components/WhyUs/WhyUs.js
@@ -8,7 +8,159 @@ function WhyUs() {
         <div className='why-us-head'>
             <h1>Why DeWork</h1>
             
-        </div>    
+        </div>   
+        <div className='why-us-box'>
+          <div className='row justify-content-center align-items-center'>
+            <div className='col-md-3'>
+              <div className='content'>
+
+                <div className='border-down'>
+                  <p>Client Fee</p>
+                  <hr/>
+                  
+                </div>
+                <div className='border-down'>
+                  <p>Freelancer Fee</p>
+                  <hr/>
+                  
+                </div>
+                <div className='border-down'>
+                  <p>Withdrawal Fee</p>
+                  <hr/>
+                  
+                </div>
+                <div className='border-down'>
+                  <p>Decentralized</p>
+                  <hr/>
+                  
+                </div>
+                <div className='border-down'>
+                  <p>Content Ownership</p>
+                  <hr/>
+                </div>
+                <div className='border-down'>
+                  <p>Efficient Escrow</p>
+                  <hr/>
+                </div>
+                <div className='border-down'>
+                  <p>No Limit on 
+task size</p>
+<hr/>
+                </div>
+                <div className='border-down'>
+                  <p>scalable dispute
+system</p>
+<hr/>
+                </div>
+                
+              </div>
+              
+            </div>
+            <div className='col-md-2'>
+              <div className='content-2'>
+              <div className=''>
+                  <p>Client Fee</p>
+                  
+                </div>
+                <div className=''>
+                  <p>Client Fee</p>
+                  
+                </div>
+                <div className=''>
+                  <p>Client Fee</p>
+                  
+                </div>
+                <div className=''>
+                  <p>Client Fee</p>
+                  
+                </div>
+                <div className=''>
+                  <p>Client Fee</p>
+                  
+                </div>
+                
+              </div>
+              
+            </div>
+            <div className='col-md-2'>
+              <div>
+              <div className=''>
+                  <p>Client Fee</p>
+                  
+                </div>
+                <div className=''>
+                  <p>Client Fee</p>
+                  
+                </div>
+                <div className=''>
+                  <p>Client Fee</p>
+                  
+                </div>
+                <div className=''>
+                  <p>Client Fee</p>
+                  
+                </div>
+                <div className=''>
+                  <p>Client Fee</p>
+                  
+                </div>
+              </div>
+              
+              </div>
+              <div className='col-md-2'>
+                <div>
+                <div className=''>
+                  <p>Client Fee</p>
+                  
+                </div>
+                <div className=''>
+                  <p>Client Fee</p>
+                  
+                </div>
+                <div className=''>
+                  <p>Client Fee</p>
+                  
+                </div>
+                <div className=''>
+                  <p>Client Fee</p>
+                  
+                </div>
+                <div className=''>
+                  <p>Client Fee</p>
+                  
+                </div>
+                </div>
+              
+              </div>
+              <div className='col-md-2'>
+                <div>
+                <div className=''>
+                  <p>Client Fee</p>
+                  
+                </div>
+                <div className=''>
+                  <p>Client Fee</p>
+                  
+                </div>
+                <div className=''>
+                  <p>Client Fee</p>
+                  
+                </div>
+                <div className=''>
+                  <p>Client Fee</p>
+                  
+                </div>
+                <div className=''>
+                  <p>Client Fee</p>
+                  
+                </div>
+                </div>
+              
+              </div>
+            
+          </div>
+          
+        </div> 
         </div>
         
         
