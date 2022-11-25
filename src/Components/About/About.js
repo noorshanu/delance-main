a8e575805d1d32da33254af180b72674fdd7d5fa
noor alam <51040978+noorshanu@users.noreply.github.com>
2022-11-25 14:12:52 +0530

    about

--- src/Components/About/About.js
+++ src/Components/About/About.js
@@ -7,10 +7,10 @@ function About() {
       <div className="container">
         <div className="row">
           <div className="col-md-6">
-            <h1>About Us</h1>
+            <h1 className="about-head-1">About Us</h1>
             <div className="about-topic">
-              <div className="d-flex gap-4">
-                <div>
+              <div className="d-flex gap-4 ab-pd">
+                <div className="ab-img">
                 <img src="https://ik.imagekit.io/cforcrypto/Dework/Rectangle_21.png?ik-sdk-version=javascript-1.4.3&updatedAt=1669013135494" alt=""/>
                 </div>
                 <div>
@@ -21,8 +21,8 @@ function About() {
                 
               </div>
 
-              <div className="d-flex gap-4">
-                <div>
+              <div className="d-flex gap-4 ab-pd">
+                <div className="ab-img">
                   <img src="https://ik.imagekit.io/cforcrypto/Dework/Rectangle_21.png?ik-sdk-version=javascript-1.4.3&updatedAt=1669013135494" alt=""/>
                 </div>
                 <div>
@@ -33,8 +33,8 @@ function About() {
                 
               </div>
 
-              <div className="d-flex gap-4">
-                <div>
+              <div className="d-flex gap-4 ab-pd">
+                <div className="ab-img">
                 <img src="https://ik.imagekit.io/cforcrypto/Dework/Rectangle_21.png?ik-sdk-version=javascript-1.4.3&updatedAt=1669013135494" alt=""/>
                 </div>
                 <div>
@@ -49,7 +49,7 @@ function About() {
           </div>
 
           <div className="col-md-6">
-            <img src="https://ik.imagekit.io/cforcrypto/Dework/Asset_2.png?ik-sdk-version=javascript-1.4.3&updatedAt=1669269508409" alt=""/>
+            <img src="https://ik.imagekit.io/cforcrypto/Dework/Asset_2.png?ik-sdk-version=javascript-1.4.3&updatedAt=1669269508409" alt="" className="ab-2-img"/>
             <div className="about-details">
               <p>
               Dework is a freelancing and Recruitment platform based on Web3. DeWork is one such decentralized platform that is redefining how freelancers connect with potential employers and buyers. Based on Web3, we are attempting to create a new freelance economy that removes intermediaries, gives freelancers full ownership of their work, and ensures the ownership of purchased digital work for buyers. It entails a metaverse with micro-communities where buyers and sellers of creative works can meet, discuss ideas, network, and form bonds, and an NFT marketplace. 
