f30b3a547905c29155002933184eec71a90237f2
noor alam <51040978+noorshanu@users.noreply.github.com>
2022-11-25 22:56:08 +0530

    new

--- src/Components/Partners/Partners.js
+++ src/Components/Partners/Partners.js
@@ -10,25 +10,25 @@ function Partners() {
         </div>
 
         <div className='row justify-content-center align-items-center'>
-        <div className='col-md-3'>
+        <div className='col-md-3 col-6'>
           <div className='text-end '>
             <img src="https://ik.imagekit.io/cforcrypto/Dework/features/Fox_new.png?ik-sdk-version=javascript-1.4.3&updatedAt=1669269583354" alt="" className='fox'/>
             
           </div>
           </div>    
-          <div className='col-md-3'>
+          <div className='col-md-3 col-6'>
           <div className='part-img-box'>
             <img src="https://ik.imagekit.io/cforcrypto/Dework/features/Business_insider.png?ik-sdk-version=javascript-1.4.3&updatedAt=1669269583817" alt="" className='insider'/>
             
           </div>
           </div>    
-          <div className='col-md-3'>
+          <div className='col-md-3 col-6'>
           <div className=''>
             <img src="https://ik.imagekit.io/cforcrypto/Dework/features/Bloomberg.png?ik-sdk-version=javascript-1.4.3&updatedAt=1669269583303" alt="" className='insider'/>
             
           </div>
           </div>    
-          <div className='col-md-3'>
+          <div className='col-md-3 col-6'>
           <div className='text-start'>
             <img src="https://ik.imagekit.io/cforcrypto/Dework/features/nbc.png?ik-sdk-version=javascript-1.4.3&updatedAt=1669269583611" alt=""  className='fox'/>
             
@@ -37,14 +37,14 @@ function Partners() {
 
 
           {/* second-row */}
-          <div className='col-md-4'>
+          <div className='col-md-4 col-6'>
           <div className='text-end'>
             <img src="https://ik.imagekit.io/cforcrypto/Dework/features/entrepreneur.png?ik-sdk-version=javascript-1.4.3&updatedAt=1669269582176" alt="" className='enter'/>
             
           </div>
           </div> 
 
-          <div className='col-md-3'>
+          <div className='col-md-3 col-6'>
           <div className='part-img-box'>
             <img src="https://ik.imagekit.io/cforcrypto/Dework/features/yahoo.png?ik-sdk-version=javascript-1.4.3&updatedAt=1669269582184" alt="" className='enter'/>
             
@@ -52,7 +52,7 @@ function Partners() {
           </div> 
 
 
-          <div className='col-md-4'>
+          <div className='col-md-4 col-6'>
           <div className='text-start'>
             <img src="https://ik.imagekit.io/cforcrypto/Dework/features/coindesk.png?ik-sdk-version=javascript-1.4.3&updatedAt=1669269582136" alt="" className='enter'/>
             
@@ -62,13 +62,13 @@ function Partners() {
 
           {/* 3rd-row */}
 
-          <div className='col-md-6'>
+          <div className='col-md-6 col-6'>
           <div className='text-end'>
             <img src="https://ik.imagekit.io/cforcrypto/Dework/features/Mkt_watch.png?ik-sdk-version=javascript-1.4.3&updatedAt=1669269583547" alt="" className='enter'/>
             
           </div>
           </div> 
-          <div className='col-md-6'>
+          <div className='col-md-6 col-6'>
           <div className='text-start'>
             <img src="https://ik.imagekit.io/cforcrypto/Dework/features/seeking_alpha.png?ik-sdk-version=javascript-1.4.3&updatedAt=1669269583686" alt="" className='enter'/>
             
