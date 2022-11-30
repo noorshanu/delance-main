7e44144dbbde5b2e3d9ec55590b05673197ab24d
noor alam <51040978+noorshanu@users.noreply.github.com>
2022-11-30 16:05:12 +0530

    new

--- src/Components/Partners/Partners.js
+++ src/Components/Partners/Partners.js
@@ -1,5 +1,8 @@
-import React from 'react'
+import { Swiper, SwiperSlide } from "swiper/react";
 import './Part.css'
+import "swiper/css";
+import "swiper/css/navigation";
+import { Navigation } from "swiper";
 
 function Partners() {
   return (
@@ -8,7 +11,8 @@ function Partners() {
         <div className='part-head text-center'>
           <h1>Featured In</h1>
         </div>
-
+        <Swiper navigation={true} modules={[Navigation]} className="mySwiper">
+        <SwiperSlide>
         <div className='row justify-content-center align-items-center'>
         <div className='col-md-3 col-6'>
           <div className='text-end '>
@@ -34,47 +38,65 @@ function Partners() {
             
           </div>
           </div> 
-
-
-          {/* second-row */}
-          <div className='col-md-4 col-6'>
-          <div className='text-end'>
-            <img src="https://ik.imagekit.io/cforcrypto/Dework/features/entrepreneur.png?ik-sdk-version=javascript-1.4.3&updatedAt=1669269582176" alt="" className='enter'/>
-            
           </div>
-          </div> 
 
-          <div className='col-md-3 col-6'>
-          <div className='part-img-box'>
-            <img src="https://ik.imagekit.io/cforcrypto/Dework/features/yahoo.png?ik-sdk-version=javascript-1.4.3&updatedAt=1669269582184" alt="" className='enter'/>
-            
-          </div>
-          </div> 
+        </SwiperSlide>
+        <SwiperSlide>
+        <div className='row justify-content-center align-items-center'>
+       
 
 
-          <div className='col-md-4 col-6'>
-          <div className='text-start'>
-            <img src="https://ik.imagekit.io/cforcrypto/Dework/features/coindesk.png?ik-sdk-version=javascript-1.4.3&updatedAt=1669269582136" alt="" className='enter'/>
-            
-          </div>
-          </div> 
+       {/* second-row */}
+       <div className='col-md-4 col-6'>
+       <div className='text-end'>
+         <img src="https://ik.imagekit.io/cforcrypto/Dework/features/entrepreneur.png?ik-sdk-version=javascript-1.4.3&updatedAt=1669269582176" alt="" className='enter'/>
+         
+       </div>
+       </div> 
+
+       <div className='col-md-3 col-6'>
+       <div className='part-img-box'>
+         <img src="https://ik.imagekit.io/cforcrypto/Dework/features/yahoo.png?ik-sdk-version=javascript-1.4.3&updatedAt=1669269582184" alt="" className='enter'/>
+         
+       </div>
+       </div> 
 
 
-          {/* 3rd-row */}
+       <div className='col-md-4 col-6'>
+       <div className='text-start'>
+         <img src="https://ik.imagekit.io/cforcrypto/Dework/features/coindesk.png?ik-sdk-version=javascript-1.4.3&updatedAt=1669269582136" alt="" className='enter'/>
+         
+       </div>
+       </div> 
+       </div>
 
-          <div className='col-md-6 col-6'>
-          <div className='text-end'>
-            <img src="https://ik.imagekit.io/cforcrypto/Dework/features/Mkt_watch.png?ik-sdk-version=javascript-1.4.3&updatedAt=1669269583547" alt="" className='enter'/>
-            
-          </div>
-          </div> 
-          <div className='col-md-6 col-6'>
-          <div className='text-start'>
-            <img src="https://ik.imagekit.io/cforcrypto/Dework/features/seeking_alpha.png?ik-sdk-version=javascript-1.4.3&updatedAt=1669269583686" alt="" className='enter'/>
-            
-          </div>
-          </div> 
-        </div>
+        </SwiperSlide>
+        <SwiperSlide>
+        <div className='row justify-content-center align-items-center'>
+       
+
+
+         
+
+       {/* 3rd-row */}
+
+       <div className='col-md-6 col-6'>
+       <div className='text-end'>
+         <img src="https://ik.imagekit.io/cforcrypto/Dework/features/Mkt_watch.png?ik-sdk-version=javascript-1.4.3&updatedAt=1669269583547" alt="" className='enter'/>
+         
+       </div>
+       </div> 
+       <div className='col-md-6 col-6'>
+       <div className='text-start'>
+         <img src="https://ik.imagekit.io/cforcrypto/Dework/features/seeking_alpha.png?ik-sdk-version=javascript-1.4.3&updatedAt=1669269583686" alt="" className='enter'/>
+         
+       </div>
+       </div> 
+     </div>
+        </SwiperSlide>
+       
+      </Swiper>
+    
         
       </div>
       
