fc7be44359d4c44814ad0e2c17369fcc3a3d18ad
noor alam <51040978+noorshanu@users.noreply.github.com>
2022-12-03 02:55:39 +0530

    new

--- src/Components/Team/Team.js
+++ src/Components/Team/Team.js
@@ -22,7 +22,9 @@ function Team() {
         <div className="team-grid ">
           <div className="team-box">
             <div className="linked">
-              <a className="linked-in" href="/"><FaLinkedin/> </a>{" "}
+              <a className="linked-in" href="/">
+                <FaLinkedin />{" "}
+              </a>{" "}
             </div>
             <div className="text-center box-img">
               <img
@@ -47,50 +49,52 @@ function Team() {
 
           <div className="team-box">
             <div className="linked">
-              <a className="linked-in" href="/"><FaLinkedin/> </a>{" "}
+              <a className="linked-in" href="/">
+                <FaLinkedin />{" "}
+              </a>{" "}
             </div>
             <div className="text-center box-img">
               <img
-                src="https://ik.imagekit.io/cforcrypto/Dework/Team/aiimg__5__1.svg?ik-sdk-version=javascript-1.4.3&updatedAt=1670013257858"
+                src="https://ik.imagekit.io/cforcrypto/Dework/Team/aiimg_1.svg?ik-sdk-version=javascript-1.4.3&updatedAt=1670013257933"
                 alt=""
               />
-              <h2>DOMMETI</h2>
-              <p>Founder / CEO</p>
+              <h2>JAGJEET JENA</h2>
+              <p>PARTNER</p>
             </div>
             <div className="team_details">
               <p>
-                Having been in market since two decades and his experience is
-                from the tech, Engineering and investment side. Demonstrated
-                ability to identify emerging global trends and then
-                create/execute vision to build/rationalize investment businesses
-                across various asset classes and geographical markets. web3 is
-                his vision, and he is responsible for developing and building a
-                specialist team to make web3 Platform the home of freelancers.
+                CEO and Co-Founder of Battle Infinity( Metaverse Battle Games
+                Platform ). having vast Technical experience in
+                leadership.Always self motivated to find ideas, solution and
+                face challenging environment to prove skills. A creative mind
+                working alongside a superior team, on course to deliver a
+                project that exceeds all expectations.
               </p>
             </div>
           </div>
 
           <div className="team-box">
             <div className="linked">
-              <a className="linked-in" href="/"><FaLinkedin/> </a>{" "}
+              <a className="linked-in" href="/">
+                <FaLinkedin />{" "}
+              </a>{" "}
             </div>
             <div className="text-center box-img">
               <img
-                src="https://ik.imagekit.io/cforcrypto/Dework/Team/aiimg__5__1.svg?ik-sdk-version=javascript-1.4.3&updatedAt=1670013257858"
+                src="https://ik.imagekit.io/cforcrypto/Dework/Team/aiimg__2__3.svg?ik-sdk-version=javascript-1.4.3&updatedAt=1670013257959"
                 alt=""
               />
-              <h2>DOMMETI</h2>
-              <p>Founder / CEO</p>
+              <h2>ROBERTO</h2>
+              <p>PROJECT HEAD</p>
             </div>
             <div className="team_details">
               <p>
-                Having been in market since two decades and his experience is
-                from the tech, Engineering and investment side. Demonstrated
-                ability to identify emerging global trends and then
-                create/execute vision to build/rationalize investment businesses
-                across various asset classes and geographical markets. web3 is
-                his vision, and he is responsible for developing and building a
-                specialist team to make web3 Platform the home of freelancers.
+                As an experienced full stack developer I've worked in the Web,
+                Blockchain for 7 years. During this time I built dozens of
+                modern and professional platforms which are making profits for
+                several companies worldwide. Building websites, crypto coins and
+                tokens, exchange platforms, defi platforms, NFT related
+                platforms, my major work fields.
               </p>
             </div>
           </div>
@@ -98,36 +102,144 @@ function Team() {
           {/* 2rd */}
 
           <div className="team-box">
+            <div className="linked">
+              <a className="linked-in" href="/">
+                <FaLinkedin />{" "}
+              </a>{" "}
+            </div>
             <div className="text-center box-img">
-              <p>hi</p>
+              <img
+                src="https://ik.imagekit.io/cforcrypto/Dework/Team/aiimg__3__1.svg?ik-sdk-version=javascript-1.4.3&updatedAt=1670013257776"
+                alt=""
+              />
+              <h2>SHOAIB</h2>
+              <p>SENIOR DEVELOPER</p>
+            </div>
+            <div className="team_details">
+              <p>
+                Experienced in Technology & Financial services sector, with 9
+                years of technical experience in strategic leadership
+                .Highlighted by consistent success, validated by tangible KPIs,
+                with a demonstrated history of working across traditional and
+                digital sets. He a creative mind working alongside a superior
+                team, on course to deliver a project that exceeds all
+                expectations .
+              </p>
             </div>
           </div>
 
           <div className="team-box">
+            <div className="linked">
+              <a className="linked-in" href="/">
+                <FaLinkedin />{" "}
+              </a>{" "}
+            </div>
             <div className="text-center box-img">
-              <p>hi</p>
+              <img
+                src="https://ik.imagekit.io/cforcrypto/Dework/Team/team1.svg?ik-sdk-version=javascript-1.4.3&updatedAt=1670013257727"
+                alt=""
+              />
+              <h2>DEBBA</h2>
+              <p>CMO</p>
+            </div>
+            <div className="team_details">
+              <p>
+              Communication and content expert ,with over 7years experience in Marketing and Business Development. she is innovator fascinated with Potential of blockchain. Debba has unique abilities and is an excellent individual with special- Mindset that distinguishes from the crowd .
+
+
+              </p>
             </div>
           </div>
 
           <div className="team-box">
+            <div className="linked">
+              <a className="linked-in" href="/">
+                <FaLinkedin />{" "}
+              </a>{" "}
+            </div>
             <div className="text-center box-img">
-              <p>hi</p>
+              <img
+                src="https://ik.imagekit.io/cforcrypto/Dework/Team/aiimg__4__2.svg?ik-sdk-version=javascript-1.4.3&updatedAt=1670013257947"
+                alt=""
+              />
+              <h2>ADNAN A RAHI</h2>
+              <p>QC/ SECURITY SPECIALIST</p>
+            </div>
+            <div className="team_details">
+              <p>
+              Addy began career as freelancer and completed 1000 plus assignments since three years , thought-out I build and secured tokens , launchpad’s , defi and other web3 applications .
+
+
+              </p>
             </div>
           </div>
           {/* 3rd */}
           <div className="team-box">
+            <div className="linked">
+              <a className="linked-in" href="/">
+                <FaLinkedin />{" "}
+              </a>{" "}
+            </div>
             <div className="text-center box-img">
-              <p>hi</p>
+              <img
+                src="https://ik.imagekit.io/cforcrypto/Dework/Team/aiimg__7__1.svg?ik-sdk-version=javascript-1.4.3&updatedAt=1670013257826"
+                alt=""
+              />
+              <h2>VISAKH</h2>
+              <p>COMMUNITY MANAGER</p>
+            </div>
+            <div className="team_details">
+              <p>
+              Visakh is a Marketing & Socials expert within the Digital marketing space while having the knowledge of an all-around-crypto individual. His involvement with crypto exceeds 3 years and has the required skills to expose Dework into hundreds of thousands of people worldwide with his unique marketing skills.
+
+
+              </p>
             </div>
           </div>
-          <div className="team-box">
+
+         <div className="team-box">
+            <div className="linked">
+              <a className="linked-in" href="/">
+                <FaLinkedin />{" "}
+              </a>{" "}
+            </div>
             <div className="text-center box-img">
-              <p>hi</p>
+              <img
+                src="https://ik.imagekit.io/cforcrypto/Dework/Team/aiimg__6__1.svg?ik-sdk-version=javascript-1.4.3&updatedAt=1670013257821"
+                alt=""
+              />
+              <h2>GURPREET SINGH</h2>
+              <p>SOCIAL MEDIA MANAGER</p>
+            </div>
+            <div className="team_details">
+              <p>
+              Social media marketer with more than five years of experience collaborating with marketing agencies and different types of startups. Creativity and data analysis are his strength, Strategist, innovative and social media.
+
+
+              </p>
             </div>
           </div>
-          <div className="team-box">
+
+
+         <div className="team-box">
+            <div className="linked">
+              <a className="linked-in" href="/">
+                <FaLinkedin />{" "}
+              </a>{" "}
+            </div>
             <div className="text-center box-img">
-              <p>hi</p>
+              <img
+                src="https://ik.imagekit.io/cforcrypto/Dework/Team/photo_2022-12-03_00-58-02.jpg?ik-sdk-version=javascript-1.4.3&updatedAt=1670013252326"
+                alt=""
+              />
+              <h2>NOOR ALAM</h2>
+              <p>FULL STACK DEVELOPER</p>
+            </div>
+            <div className="team_details">
+              <p>
+              Noor Alam is full stack web developer with 5 years experiance working in all kind of platforms , ha has very strong background and expertise in React , NodeJs and phyton he is a great team player and self starter.
+
+              </p>
             </div>
           </div>
         </div>
