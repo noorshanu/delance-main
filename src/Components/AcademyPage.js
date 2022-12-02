279d5fabb980267f2855849f7b25d721b4565725
noor alam <51040978+noorshanu@users.noreply.github.com>
2022-12-02 20:50:25 +0530

    new

--- src/Components/AcademyPage.js
+++ src/Components/AcademyPage.js
@@ -5,9 +5,9 @@ function AcademyPage() {
     <>
     <section className='academy'>
         <div className='container'>
-            <p>Academy</p>
+            <p className='ad-p'>Academy</p>
             <div className='academy-head'>
-            <h1>Bondex is an ecosystem for both crypto natives as well as those who are entering the space for the first time. Here you will find the necessary tools and materials to get up to speed on the latest developments</h1>
+            <h1>Deelance is an ecosystem for both crypto natives as well as those who are entering the space for the first time. Here you will find the necessary tools and materials to get up to speed on the latest developments</h1>
 
             </div>
             <div className='academy-btn'>
