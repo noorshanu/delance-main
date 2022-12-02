0b5a78c1c71b92ffe500cfb4dbeaadd396da2ef7
noor alam <51040978+noorshanu@users.noreply.github.com>
2022-12-02 17:15:53 +0530

    Update Jobs.js

--- src/Components/Jobs/Jobs.js
+++ src/Components/Jobs/Jobs.js
@@ -4,7 +4,14 @@ import './Jobs.css'
 function Jobs() {
   return (
     <section className='jobs'>
-        
+        <div className='container'>
+            <div className='job-head'>
+                <h1>Find Jobs on your finguretips , Access to best jobs and leading professionals in the industry
+</h1>
+
+            </div>
+
+        </div>
         </section>
   )
 }
