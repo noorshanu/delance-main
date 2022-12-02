16b766d72647227290e4025a1dd31f5a61c536e9
noor alam <51040978+noorshanu@users.noreply.github.com>
2022-12-02 19:39:50 +0530

    Update Jobs.js

--- src/Components/Jobs/Jobs.js
+++ src/Components/Jobs/Jobs.js
@@ -3,6 +3,7 @@ import './Jobs.css'
 
 function Jobs() {
   return (
+    <>
     <section className='jobs'>
         <div className='container'>
             <div className='job-head'>
@@ -10,9 +11,33 @@ function Jobs() {
 </h1>
 
             </div>
+            <div className='row'>
+                <div className='col-md-3 col-6'>
+                    
+                </div>
+                <div className='col-md-3 col-6'>
+                    
+                    </div>
+                    <div className='col-md-3 col-6'>
+                    
+                    </div>
+                    <div className='col-md-3 col-6'>
+                    
+                    </div>
+                
+            </div>
+            <div className='job-soon'>
+                <h1>Our job Portal is coming soon.....
+</h1>
+                
+            </div>
 
         </div>
         </section>
+        <section>
+            <img src="https://ik.imagekit.io/cforcrypto/Dework/find_jobs/findJob_-_updated_1.png?ik-sdk-version=javascript-1.4.3&updatedAt=1669980843877" alt=""/>
+        </section>
+      </>
   )
 }
 
