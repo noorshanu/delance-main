6d29bb5e0dfeec604d7ce7946b7947c75dd66337
noor alam <51040978+noorshanu@users.noreply.github.com>
2022-12-29 12:27:56 +0530

    new

--- src/Components/WhyUs/WhyUs.js
+++ src/Components/WhyUs/WhyUs.js
@@ -6,265 +6,79 @@ import "./Why.css";
 function WhyUs() {
   return (
     <section className="why-us">
-      <MobWhyus/>
+      <MobWhyus />
       <div className="container desktop-box-why">
         <div className="why-us-head text-center">
           <h1>Why Deelance</h1>
         </div>
-        
-        <div className="why-us-box ">
-          <div className="row justify-content-center align-items-center">
-            <div className="col-md-3">
-              <div className="content">
-                <div className="border-down">
-                  <p>Client Fee</p>
-                  <hr />
-                </div>
-                <div className="border-down">
-                  <p>Freelancer Fee</p>
-                  <hr />
-                </div>
-                <div className="border-down">
-                  <p>Withdrawal Fee</p>
-                  <hr />
-                </div>
-                <div className="border-down">
-                  <p>Decentralized</p>
-                  <hr />
-                </div>
-                <div className="border-down">
-                  <p>Content Ownership</p>
-                  <hr />
-                </div>
-                <div className="border-down">
-                  <p>Efficient Escrow</p>
-                  <hr />
-                </div>
-                <div className="border-down">
-                  <p>No Limit on task size</p>
-                  <hr />
-                </div>
-                <div className="border-down">
-                  <p>scalable dispute system</p>
-                  <hr />
-                </div>
-                <div className="border-down">
-                  <p>NFT / Metaverse</p>
-                  <hr />
-                </div>
-                <div className="border-down">
-                  <p>Staff Recruitment</p>
-                  <hr />
-                </div>
-                
-              </div>
-            </div>
-            <div className="col-md-2">
-              <div className="content-2 text-center">
-                <div className="why-content-img-head">
-                  <img
-                    src="https://ik.imagekit.io/cforcrypto/Dework/features/white_v-8.png?ik-sdk-version=javascript-1.4.3&updatedAt=1669879313608"
-                    alt=""
-                  />
-                </div>
-                <div className="">
-                  <p>2%</p>
-                  <hr />
-                </div>
-                <div className="">
-                  <p>10%</p>
-                  <hr />
-                </div>
-                <div className="">
-                  <p>Crypto/Instant</p>
-                  <hr />
-                </div>
-                <div className="">
-                  <p>Yes</p>
-                  <hr />
-                </div>
-                <div className="">
-                  <p>Yes</p>
-                  <hr />
-                </div>
-                <div className="">
-                  <p>Yes</p>
-                  <hr />
-                </div>
-                <div className="">
-                  <p>Yes</p>
-                  <hr />
-                </div>
-                <div className="">
-                  <p>Yes</p>
-                  <hr />
-                </div>
-                <div className="">
-                  <p>Yes</p>
-                  <hr />
-                </div>
-                <div className="">
-                  <p>Yes</p>
-                  <hr />
-                </div>
-              </div>
-            </div>
-            <div className="col-md-2">
-              <div className="content-3 text-center">
-                <div className="why-content-img-head">
-                  <img
-                    src="https://ik.imagekit.io/cforcrypto/Dework/image_8.png?ik-sdk-version=javascript-1.4.3&updatedAt=1669366389254"
-                    alt=""
-                  />
-                </div>
-                <div className="">
-                  <p>3%</p>
-                  <hr />
-                </div>
-                <div className="">
-                  <p>10%</p>
-                  <hr />
-                </div>
-                <div className="short-dis">
-                  <p>Min payout $100</p>
-                  <p>(2.75% fee for withdrawal for currency conversion)</p>
-                  <hr />
-                </div>
-                <div className="">
-                  <p>No</p>
-                  <hr />
-                </div>
-                <div className="">
-                  <p>No</p>
-                  <hr />
-                </div>
-                <div className="">
-                  <p>No/partial</p>
-                  <hr />
-                </div>
-                <div className="">
-                  <p>No</p>
-                  <hr />
-                </div>
-                <div className="">
-                  <p>No</p>
-                  <hr />
-                </div>
-                <div className="">
-                  <p>No</p>
-                  <hr />
-                </div>
-                <div className="">
-                  <p>No</p>
-                  <hr />
-                </div>
-              </div>
-            </div>
-            <div className="col-md-2">
-              <div className="content-3 text-center">
-                <div className="why-content-img-head">
-                  <img
-                    src="https://ik.imagekit.io/cforcrypto/Dework/image_9.png?ik-sdk-version=javascript-1.4.3&updatedAt=1669366389048"
-                    alt=""
-                  />
-                </div>
-                <div className="up-work">
-                  <p style={{marginBottom:'0px'}}>3.5%</p>
-                  {/* <p className="each">(Each milestone)</p> */}
-                  <hr />
-                </div>
-                <div className="">
-                  <p>20%</p>
-                  <hr />
-                </div>
-                <div className="short-dis">
-                  <p>Min payout $100</p>
-                  <p>(5% fee for withdrawal for currency conversion)</p>
-                  <hr />
-                </div>
-                <div className="">
-                  <p>No</p>
-                  <hr />
-                </div>
-                <div className="">
-                  <p>No</p>
-                  <hr />
-                </div>
-                <div className="">
-                  <p>No/partial</p>
-                  <hr />
-                </div>
-                <div className="">
-                  <p>No</p>
-                  <hr />
-                </div>
-                <div className="">
-                  <p>No</p>
-                  <hr />
-                </div>
-                <div className="">
-                  <p>No</p>
-                  <hr />
-                </div>
-                <div className="">
-                  <p>No</p>
-                  <hr />
-                </div>
-              </div>
-            </div>
-            <div className="col-md-2">
-              <div className="content-3">
-                <div className="why-content-img-head">
-                  <img
-                    src="https://ik.imagekit.io/cforcrypto/Dework/image_10.png?ik-sdk-version=javascript-1.4.3&updatedAt=1669366388768"
-                    alt=""
-                  />
-                </div>
-                <div className="up-work">
-                  <p>20%</p>
 
-                  <hr />
-                </div>
-                <div className="">
-                  <p>20%</p>
-                  <hr />
-                </div>
-                <div className="short-dis">
-                  <p>Min payout $100</p>
-                  <p>(5% fee for withdrawal for currency conversion)</p>
-                  <hr />
-                </div>
-                <div className="">
-                  <p>No</p>
-                  <hr />
-                </div>
-                <div className="">
-                  <p>No</p>
-                  <hr />
-                </div>
-                <div className="">
-                  <p>No/partial</p>
-                  <hr />
-                </div>
-                <div className="">
-                  <p>No</p>
-                  <hr />
-                </div>
-                <div className="">
-                  <p>No</p>
-                  <hr />
-                </div>
-                <div className="">
-                  <p>No</p>
-                  <hr />
-                </div>
-                <div className="">
-                  <p>No</p>
-                  <hr />
-                </div>
-              </div>
-            </div>
-          </div>
+        <div className="why-us-box ">
+          <table className="table">
+            <thead>
+              <tr>
+                <th scope="col">#</th>
+                <th scope="col">
+                  <div className="content-2">
+                    <img
+                      src="https://ik.imagekit.io/cforcrypto/Dework/logo_new.png?ik-sdk-version=javascript-1.4.3&updatedAt=1669805675437"
+                      alt=""
+                    />
+                  </div>
+                </th>
+                <th scope="col">
+                <div className="content-2">
+                    <img
+                      src="https://ik.imagekit.io/cforcrypto/Dework/logo_new.png?ik-sdk-version=javascript-1.4.3&updatedAt=1669805675437"
+                      alt=""
+                    />
+                  </div>
+                </th>
+                <th scope="col">
+                <div className="content-2">
+                    <img
+                      src="https://ik.imagekit.io/cforcrypto/Dework/logo_new.png?ik-sdk-version=javascript-1.4.3&updatedAt=1669805675437"
+                      alt=""
+                    />
+                  </div>
+                </th>
+                <th scope="col">
+                <div className="content-2">
+                    <img
+                      src="https://ik.imagekit.io/cforcrypto/Dework/logo_new.png?ik-sdk-version=javascript-1.4.3&updatedAt=1669805675437"
+                      alt=""
+                    />
+                  </div>
+                </th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <th scope="row">1</th>
+                <td>Mark</td>
+                <td>Otto</td>
+                <td>@mdo</td>
+                <td>@twitter</td>
+              </tr>
+              <tr>
+                <th scope="row">2</th>
+                <td>Jacob</td>
+                <td>Thornton</td>
+                <td>@fat</td>
+              </tr>
+              <tr>
+                <th scope="row">3</th>
+                <td colspan="2">Larry the Bird</td>
+                <td>@twitter</td>
+                <td>@twitter</td>
+              </tr>
+              <tr>
+                <th scope="row">4</th>
+                <td colspan="2">Larry the Bird</td>
+                <td>@twitter</td>
+                <td>@twitter</td>
+              </tr>
+            </tbody>
+          </table>
         </div>
       </div>
     </section>
