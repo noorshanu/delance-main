6633713c2d0542e975e1a9eac24c5f36ab4bf187
noor alam <51040978+noorshanu@users.noreply.github.com>
2022-12-29 16:55:40 +0530

    Update WhyUs.js

--- src/Components/WhyUs/WhyUs.js
+++ src/Components/WhyUs/WhyUs.js
@@ -9,7 +9,9 @@ function WhyUs() {
       <MobWhyus />
       <div className="container desktop-box-why">
         <div className="why-us-head text-center">
-          <h1>Why <span className="green">Deelance</span> </h1>
+          <h1>
+            Why <span className="green">Deelance</span>{" "}
+          </h1>
         </div>
 
         <div className="why-us-box ">
@@ -26,25 +28,25 @@ function WhyUs() {
                   </div>
                 </th>
                 <th scope="col">
-                <div className="content-2">
+                  <div className="content-2">
                     <img
-                      src="https://ik.imagekit.io/cforcrypto/Dework/logo_new.png?ik-sdk-version=javascript-1.4.3&updatedAt=1669805675437"
+                      src="https://ik.imagekit.io/cforcrypto/Dework/image_8.png?ik-sdk-version=javascript-1.4.3&updatedAt=1669366389254"
                       alt=""
                     />
                   </div>
                 </th>
                 <th scope="col">
-                <div className="content-2">
+                  <div className="content-2">
                     <img
-                      src="https://ik.imagekit.io/cforcrypto/Dework/logo_new.png?ik-sdk-version=javascript-1.4.3&updatedAt=1669805675437"
+                      src="https://ik.imagekit.io/cforcrypto/Dework/image_9.png?ik-sdk-version=javascript-1.4.3&updatedAt=1669366389048"
                       alt=""
                     />
                   </div>
                 </th>
                 <th scope="col">
-                <div className="content-2">
+                  <div className="content-2">
                     <img
-                      src="https://ik.imagekit.io/cforcrypto/Dework/logo_new.png?ik-sdk-version=javascript-1.4.3&updatedAt=1669805675437"
+                      src="https://ik.imagekit.io/cforcrypto/Dework/image_10.png?ik-sdk-version=javascript-1.4.3&updatedAt=1669366388768"
                       alt=""
                     />
                   </div>
@@ -55,74 +57,87 @@ function WhyUs() {
               <tr>
                 <th scope="row">
                   <p>Client Fee</p>
-                   </th>
-                <td>Mark</td>
-                <td>Otto</td>
-                <td>@mdo</td>
-                <td>@twitter</td>
+                </th>
+                <td>2%</td>
+                <td>3%</td>
+                <td>3.5%</td>
+                <td>20%</td>
               </tr>
               <tr>
-                <th scope="row"><p>Freelancer Fee</p> </th>
-                <td>Jacob</td>
-                <td>Thornton</td>
-                <td>@fat</td>
-                <td>@twitter</td>
+                <th scope="row">
+                  <p>Freelancer Fee</p>{" "}
+                </th>
+                <td>10%</td>
+                <td>10%</td>
+                <td>20%</td>
+                <td>20%</td>
               </tr>
               <tr>
-                <th scope="row"><p>Withdrawal Fee</p></th>
-                <td  >Larry the Bird</td>
-                <td>@twitter</td>
-                <td>@twitter</td>
-                <td>@twitter</td>
+                <th scope="row">
+                  <p>Withdrawal Fee</p>
+                </th>
+                <td>Crypto/Instant</td>
+                <td>
+                  Min Payout $100 (2.75% Fee For Withdrawal For Currency
+                  Conversion)
+                </td>
+                <td>
+                  Min Payout $100 (5% Fee For Withdrawal For Currency
+                  Conversion)
+                </td>
+                <td>
+                  Min Payout $100 (5% Fee For Withdrawal For Currency
+                  Conversion)
+                </td>
               </tr>
               <tr>
                 <th scope="row">Decentralized</th>
-                <td  >Larry the Bird</td>
+                <td>Larry the Bird</td>
                 <td>@twitter</td>
                 <td>@twitter</td>
                 <td>@twitter</td>
               </tr>
               <tr>
-                <th scope="row">Decentralized</th>
-                <td  >Larry the Bird</td>
+                <th scope="row">Content Ownership</th>
+                <td>Larry the Bird</td>
                 <td>@twitter</td>
                 <td>@twitter</td>
                 <td>@twitter</td>
               </tr>
               <tr>
-                <th scope="row">Decentralized</th>
-                <td  >Larry the Bird</td>
+                <th scope="row">Efficient Escrow</th>
+                <td>Larry the Bird</td>
                 <td>@twitter</td>
                 <td>@twitter</td>
                 <td>@twitter</td>
               </tr>
 
               <tr>
-                <th scope="row">Decentralized</th>
-                <td  >Larry the Bird</td>
+                <th scope="row">No Limit On Task Size</th>
+                <td>Larry the Bird</td>
                 <td>@twitter</td>
                 <td>@twitter</td>
                 <td>@twitter</td>
               </tr>
 
               <tr>
-                <th scope="row">Decentralized</th>
-                <td  >Larry the Bird</td>
+                <th scope="row">Scalable Dispute System</th>
+                <td>Larry the Bird</td>
                 <td>@twitter</td>
                 <td>@twitter</td>
                 <td>@twitter</td>
               </tr>
 
               <tr>
-                <th scope="row">Decentralized</th>
-                <td  >Larry the Bird</td>
+                <th scope="row">NFT / Metaverse</th>
+                <td>Larry the Bird</td>
                 <td>@twitter</td>
                 <td>@twitter</td>
                 <td>@twitter</td>
               </tr>
               <tr>
-                <th scope="row">Decentralized</th>
-                <td  >Larry the Bird</td>
+                <th scope="row">Staff Recruitment</th>
+                <td>Larry the Bird</td>
                 <td>@twitter</td>
                 <td>@twitter</td>
                 <td>@twitter</td>
