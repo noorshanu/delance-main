64b5609a43d88fa7f163839068acf7cf923e1e8e
noor alam <51040978+noorshanu@users.noreply.github.com>
2022-12-30 15:28:32 +0530

    fg

--- src/Components/Roadmap/Raod.js
+++ src/Components/Roadmap/Raod.js
@@ -1,18 +1,6 @@
-import { Swiper, SwiperSlide } from "swiper/react";
-
-// Import Swiper styles
-import "swiper/css";
-import "swiper/css/effect-coverflow";
-import "swiper/css/pagination";
 import './Road.css'
-import "swiper/css/navigation";
-import { Navigation } from "swiper";
 
-import {FaCheck} from 'react-icons/fa'
-import {GoPrimitiveDot} from 'react-icons/go'
 
-// import required modules
-import { Autoplay,EffectCoverflow, Pagination } from "swiper";
 
 export default function Raod() {
   return (
@@ -20,106 +8,12 @@ export default function Raod() {
     <section className="roadmap">
         <div className="container">
             <div className="road-head">
-                <h1>ROADMAP</h1>
+                <h1 className="green">ROADMAP</h1>
                 
             </div>
             <div className="road-slide">
-            <Swiper
-        effect={"coverflow"}
-        grabCursor={true}
-        centeredSlides={true}
-        slidesPerView={"auto"}
-        coverflowEffect={{
-          rotate: 50,
-          stretch: 0,
-          depth: 100,
-          modifier: 1,
-          slideShadows: false,
-        }}
-        autoplay={{
-          delay: 3500,
-          disableOnInteraction: false,
-        }}
-        pagination={true}
-        navigation={true}
-        modules={[EffectCoverflow, Pagination,Navigation,Autoplay,]}
-        className="mySwiper"
-      >
-        <SwiperSlide>
-          <div className="road-box">
-          <div className="road-top">
-              <h2>Q4 2022 </h2>
-              <p>( Nov-Dec )</p>
-            </div>
-            <div className="road-list">
-            <p><FaCheck className=""/> Initial Project Plan </p>
-              <p><FaCheck /> website Design  </p>
-              <p><FaCheck/> Website Live </p>
-              <p><FaCheck/> Initial Smart-contracts Deployment  </p>
-              
-             
-            </div>
-
-          </div>
-        </SwiperSlide>
-        <SwiperSlide>
-        <div className="road-box">
-        
-            <div className="road-top">
-              <h2>Q1 2023 </h2>
-              <p> ( Jan - March )</p>
-            </div>
-            <div className="road-list">
-            <p><GoPrimitiveDot/> Private Sale  </p>
-              <p><GoPrimitiveDot/> Project Launch  </p>
-              <p><GoPrimitiveDot/> Metaverse Demo   </p>
-              <p><GoPrimitiveDot/> CMC and CG Listing   </p>
-
-             
-              
-            </div>
-          </div>
-        </SwiperSlide>
-        <SwiperSlide>
-        <div className="road-box">
-        <div className="road-top">
-              <h2>Q2 2023</h2>
-              <p>( April -June )   </p>
-            </div>
-            <div className="road-list">
-            <p><GoPrimitiveDot /> NFT Launch  </p>
-              <p><GoPrimitiveDot/> NFT MarketPlace </p>
-              <p><GoPrimitiveDot/> Freelance Platform Launch </p>
-              <p><GoPrimitiveDot className=""/> Exchanges Listing  </p>
-              <p><GoPrimitiveDot/> Metaverse Development    </p>
-
-     
-            
-            </div>
-      
-          </div>
-        </SwiperSlide>
-        <SwiperSlide>
-        <div className="road-box">
-        <div className="road-top">
-              <h2>Q3 2023</h2>
-              <p>( July -Sept )   </p>
-            </div>
-            <div className="road-list">
-            
-
-              <p><GoPrimitiveDot /> App development   </p>
-              <p><GoPrimitiveDot/> Metaverse Integration to Main Platform </p>
-              <p><GoPrimitiveDot/> Further Development   </p>
-            
-            </div>
-      
-          </div>
-        </SwiperSlide>
+              <img src="https://ik.imagekit.io/cforcrypto/Dework/Mask_group.svg?ik-sdk-version=javascript-1.4.3&updatedAt=1672394255186" alt=""/>
    
-   
-  
-      </Swiper>
 
             </div>
         </div>
--- src/Components/Token/Tokenomic.js
+++ src/Components/Token/Tokenomic.js
@@ -9,10 +9,10 @@ function Tokenomic() {
             <h1>Tokenomics</h1>
 
           </div>
-        <img src="https://ik.imagekit.io/cforcrypto/Dework/features/-w_logo3-8.png?ik-sdk-version=javascript-1.4.3&updatedAt=1669923773737" alt="" className='token-img'/>
+        <img src="https://ik.imagekit.io/cforcrypto/Dework/Group_1261152779.png?ik-sdk-version=javascript-1.4.3&updatedAt=1672393264251" alt="" className='token-img'/>
         </div>
         <div style={{margin:'auto'}} className='main-sub-btn'>
-            <a href="#team" className="explore-btn">BUY NOW</a>
+            <a href="#team" className="demo-btn">BUY NOW</a>
             </div>
         </section>
   )
