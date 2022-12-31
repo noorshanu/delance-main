feb68790e085be602a543c48d7449a70d983834f
noor alam <51040978+noorshanu@users.noreply.github.com>
2022-12-31 14:29:12 +0530

    dfg

--- src/Components/Roadmap/Raod.js
+++ src/Components/Roadmap/Raod.js
@@ -1,29 +1,144 @@
-import './Road.css'
-
-
+import "./Road.css";
+import { GoPrimitiveDot } from "react-icons/go";
 
 export default function Raod() {
   return (
     <>
-    <section className="roadmap">
+      <section className="roadmap">
         <div className="container">
-            <div className="road-head">
-                <h1 className="green">ROADMAP</h1>
-                
-            </div>
-            <div className="road-slide">
-              <img src="https://ik.imagekit.io/cforcrypto/Dework/Mask_group.svg?ik-sdk-version=javascript-1.4.3&updatedAt=1672394255186" alt=""/>
-   
+          <div className="road-head">
+            <h1 className="green">ROADMAP</h1>
+          </div>
+          <div className="road-slide">
+            <img
+              src="https://ik.imagekit.io/cforcrypto/Dework/Mask_group.svg?ik-sdk-version=javascript-1.4.3&updatedAt=1672394255186"
+              alt=""
+            />
+          </div>
+          <div className="grid-road">
 
+         
+          <div className="box-road-1">
+            <div className="text-center rb-img">
+              <img
+                src="https://ik.imagekit.io/cforcrypto/Dework/Group_1261152793.svg?ik-sdk-version=javascript-1.4.3&updatedAt=1672394848621"
+                alt=""
+                style={{width:'auto'}}
+              />
             </div>
-            <div className='box-road-1'>
-
+            <div className="text-center">
+            <h1>Q4 2022</h1>
+            <p>(Nov - Dec)</p>
             </div>
+            
+            <div className="rd-b">
+              <p>
+                <GoPrimitiveDot /> Initial Project Plan{" "}
+              </p>
+              <p>
+                <GoPrimitiveDot /> Initial Project Plan{" "}
+              </p>
+              <p>
+                <GoPrimitiveDot /> Initial Project Plan{" "}
+              </p>
+              <p>
+                <GoPrimitiveDot /> Initial Project Plan{" "}
+              </p>
+            </div>
+          </div>
 
+
+          <div className="box-road-1">
+            <div className="text-center rb-img">
+              <img
+                src="https://ik.imagekit.io/cforcrypto/Dework/Group_1261152793.svg?ik-sdk-version=javascript-1.4.3&updatedAt=1672394848621"
+                alt=""
+                style={{width:'auto'}}
+              />
+            </div>
+            <div className="text-center">
+            <h1>Q4 2022</h1>
+            <p>(Nov - Dec)</p>
+            </div>
+            
+            <div className="rd-b">
+              <p>
+                <GoPrimitiveDot /> Initial Project Plan{" "}
+              </p>
+              <p>
+                <GoPrimitiveDot /> Initial Project Plan{" "}
+              </p>
+              <p>
+                <GoPrimitiveDot /> Initial Project Plan{" "}
+              </p>
+              <p>
+                <GoPrimitiveDot /> Initial Project Plan{" "}
+              </p>
+            </div>
+          </div>
+
+
+          <div className="box-road-1">
+            <div className="text-center rb-img">
+              <img
+                src="https://ik.imagekit.io/cforcrypto/Dework/Group_1261152793.svg?ik-sdk-version=javascript-1.4.3&updatedAt=1672394848621"
+                alt=""
+                style={{width:'auto'}}
+              />
+            </div>
+            <div className="text-center">
+            <h1>Q4 2022</h1>
+            <p>(Nov - Dec)</p>
+            </div>
+            
+            <div className="rd-b">
+              <p>
+                <GoPrimitiveDot /> Initial Project Plan{" "}
+              </p>
+              <p>
+                <GoPrimitiveDot /> Initial Project Plan{" "}
+              </p>
+              <p>
+                <GoPrimitiveDot /> Initial Project Plan{" "}
+              </p>
+              <p>
+                <GoPrimitiveDot /> Initial Project Plan{" "}
+              </p>
+            </div>
+          </div>
+
+
+          <div className="box-road-1">
+            <div className="text-center rb-img">
+              <img
+                src="https://ik.imagekit.io/cforcrypto/Dework/Group_1261152793.svg?ik-sdk-version=javascript-1.4.3&updatedAt=1672394848621"
+                alt=""
+                style={{width:'auto'}}
+              />
+            </div>
+            <div className="text-center">
+            <h1>Q4 2022</h1>
+            <p>(Nov - Dec)</p>
+            </div>
+            
+            <div className="rd-b">
+              <p>
+                <GoPrimitiveDot /> Initial Project Plan{" "}
+              </p>
+              <p>
+                <GoPrimitiveDot /> Initial Project Plan{" "}
+              </p>
+              <p>
+                <GoPrimitiveDot /> Initial Project Plan{" "}
+              </p>
+              <p>
+                <GoPrimitiveDot /> Initial Project Plan{" "}
+              </p>
+            </div>
+          </div>
+          </div>
         </div>
-        
-    </section>
-    
+      </section>
     </>
-  )
+  );
 }
