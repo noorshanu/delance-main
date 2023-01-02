dd08a1655300a7790d52a4a78df65490354c71ce
noor alam <51040978+noorshanu@users.noreply.github.com>
2023-01-02 16:41:31 +0530

    fg

--- src/Components/Token/Tokenomic.js
+++ src/Components/Token/Tokenomic.js
@@ -16,8 +16,8 @@ function Tokenomic() {
               <div className='to-1 text-end'>
                 <p>Team - 10% <img src="https://ik.imagekit.io/cforcrypto/Dework/token/Ellipse_22-6.png?ik-sdk-version=javascript-1.4.3&updatedAt=1672652602964" alt=""/> </p>
                 <p>Reserved/Ecosystem - 15% <img src="https://ik.imagekit.io/cforcrypto/Dework/token/Ellipse_22-4.png?ik-sdk-version=javascript-1.4.3&updatedAt=1672652603018" alt=""/></p>
-                <p>Cex - 16% <span className='deep-blue'><BiCircle/></span></p>
-                <p>Advisor/Partnership - 6% <span className='green'><BiCircle/></span></p>
+                <p>Cex - 16% <img src="https://ik.imagekit.io/cforcrypto/Dework/token/Ellipse_22-7.png?ik-sdk-version=javascript-1.4.3&updatedAt=1672652602977" alt=""/></p>
+                <p>Advisor/Partnership - 6% <img src="https://ik.imagekit.io/cforcrypto/Dework/token/Ellipse_22-5.png?ik-sdk-version=javascript-1.4.3&updatedAt=1672652602962" alt=""/></p>
 
               </div>
               
@@ -32,10 +32,10 @@ function Tokenomic() {
 
               <div className='col-md-4'>
               <div className='to-2 text-start'>
-                <p><span><BiCircle/></span> Rewards / Airdrop - 2%  </p>
-                <p> <span><BiCircle/></span> Liquidity - 15%</p>
-                <p><span><BiCircle/></span> Presale - 33%</p>
-                <p><span><BiCircle/></span> Private - 3%</p>
+                <p><img src="https://ik.imagekit.io/cforcrypto/Dework/token/Ellipse_22.png?ik-sdk-version=javascript-1.4.3&updatedAt=1672652602987" alt=""/> Rewards / Airdrop - 2%  </p>
+                <p><img src="https://ik.imagekit.io/cforcrypto/Dework/token/Ellipse_22-2.png?ik-sdk-version=javascript-1.4.3&updatedAt=1672652602973" alt=""/> Liquidity - 15%</p>
+                <p><img src="https://ik.imagekit.io/cforcrypto/Dework/token/Ellipse_22-1.png?ik-sdk-version=javascript-1.4.3&updatedAt=1672652602976" alt=""/> Presale - 33%</p>
+                <p><img src="https://ik.imagekit.io/cforcrypto/Dework/token/Ellipse_22-3.png?ik-sdk-version=javascript-1.4.3&updatedAt=1672652602935" alt=""/> Private - 3%</p>
 
               </div>
               
